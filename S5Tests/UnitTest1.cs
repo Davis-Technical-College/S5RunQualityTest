310708c8e5ff441c2c6b3efe8e2f09eb702bbfd2
George Ray <George.Ray@davistech.edu>
2024-02-28 14:13:00 -0700

    start repo

---
+++ S5Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace S5Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
